bed9e0306b084489917c660d184cbc75a552c406
Andrea Walsh <61071442+awalsh0517@users.noreply.github.com>
2021-01-13 12:40:59 -0500

    Adding Images

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 window.addEventListener('load', () => {
   let long;
   let lat;
+  let temperatureDescription = document.querySelector('.temperature-description')
+  let temperatureDegree = document.querySelector('.temperature-degree')
+  let locationTimezone = document.querySelector('.location-timezone')
+  let locationIcon = document.querySelector('.weather-icon')
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-      lon = position.coords.longitude;
+      long = position.coords.longitude;
       lat = position.coords.latitude;
 
-      const proxy = 'https://cors-anywhere.herokuapp.com/';
-      const api = `${proxy}api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={1040d1c20eb508d019357fa863ff80ee
-      }`
+      const proxy = `http://cors-anywhere.herokuapp.com/`;
+      const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=1040d1c20eb508d019357fa863ff80ee`;
 
 
       fetch(api)
         .then(response => {
-          return response.json()
+          return response.json();
         })
         .then(data => {
-          console.log(data)
+          console.log(data);
+
+          const { temp } = data.main
+          temperatureDegree.textContent = temp
+
+          const { description, icon } = data.weather[0]
+          temperatureDescription.textContent = description
+          locationIcon.innerHTML = `<img src="icons/${icon}.png">`
+
+          locationTimezone.textContent = data.name
+
         })
     })
   }
+
+
+  // function setIcons(icon, iconId)
+  // const skyscons = new skyscons({ color: "white" })
+
+
   // else {
   //   h1.textContent = "This could not load please enable location services"
   // }
