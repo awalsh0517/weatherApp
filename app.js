e9837213faadbcda8f73a3d8dbb1b4f3ec73d0e6
Andrea Walsh <61071442+awalsh0517@users.noreply.github.com>
2021-01-13 12:59:46 -0500

    Change between C and F added

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ window.addEventListener('load', () => {
   let locationTimezone = document.querySelector('.location-timezone')
   let locationIcon = document.querySelector('.weather-icon')
 
+  let temperatureSection = document.querySelector('.temperature')
+  const temperatureSpan = document.querySelector('.temperature span')
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude;
       lat = position.coords.latitude;
 
       const proxy = `http://cors-anywhere.herokuapp.com/`;
-      const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=1040d1c20eb508d019357fa863ff80ee`;
+      const api = `${proxy}https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=1040d1c20eb508d019357fa863ff80ee`;
 
 
       fetch(api)
@@ -30,6 +33,20 @@ window.addEventListener('load', () => {
           locationIcon.innerHTML = `<img src="icons/${icon}.png">`
 
           locationTimezone.textContent = data.name
+
+          // formula for celsius
+          let celsius = (temp - 32) * (5 / 9)
+
+          // change temp to cel/fer
+          temperatureSection.addEventListener('click', () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C"
+              temperatureDegree.textContent = Math.floor(celsius)
+            } else {
+              temperatureSpan.textContent = "F"
+              temperatureDegree.textContent = Math.floor(temp)
+            }
+          })
 
         })
     })
